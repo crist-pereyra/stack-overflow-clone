21b3b698dc02055ab7c5be56cbfef0b7810afa44
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-04-21 22:32:08 -0400

    Add Auth

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,4 +0,0 @@
-export const SignIn = () => {
-  return <div>sign-in</div>;
-};
-export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,4 +0,0 @@
-export const SignUp = () => {
-  return <div>sign-up</div>;
-};
-export default SignUp;
