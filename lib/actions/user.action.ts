0c9f942ab4bfbb3a134f02e550907e9cadc4f28a
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-05-30 12:27:54 -0400

    Add webhooks user creation

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from './shared.types';
+import { revalidatePath } from 'next/cache';
+import Question from '@/database/question.model';
 
 export const getUserById = async (params: any) => {
   try {
@@ -10,6 +17,59 @@ export const getUserById = async (params: any) => {
 
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const createUser = async (userData: CreateUserParams) => {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateUser = async (params: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteUser = async (params: DeleteUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Delete user from database and questions, answers, comments, etc.
+
+    // get user question ids
+    const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+      '_id'
+    );
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // delete user answers
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
