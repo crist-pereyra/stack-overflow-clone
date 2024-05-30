ec5dc652c025d4f4edffdf8f78fc96c492c3705d
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-05-30 15:09:39 -0400

    Add api webhook

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -9,7 +9,7 @@ const AskQuestion = async () => {
   const userId = '123456789';
   if (!userId) redirect('/sign-in');
   const mongoUser = await getUserById({ userId });
-  console.log('user', mongoUser);
+
   return (
     <div>
       <h1 className='h1-bold text-dark100_light900'>Ask question</h1>
