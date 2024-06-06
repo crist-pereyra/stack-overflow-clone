af5fb202be5d3954e95ce6d0afa0ddb5d4bb42e6
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-06-05 22:31:02 -0400

    Add interaction view

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { downvoteAnswer, upvoteAnswer } from '@/lib/actions/answer.action';
+import { viewQuestion } from '@/lib/actions/interaction.action';
 import {
   downvoteQuestion,
   upvoteQuestion,
@@ -7,9 +8,8 @@ import {
 import { toggleSaveQuestion } from '@/lib/actions/user.action';
 import { formatBigNumber } from '@/lib/utils';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-// import { useRouter } from 'next/router';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 interface Props {
   type: string;
@@ -32,7 +32,7 @@ const Votes = ({
   hasSaved,
 }: Props) => {
   const pathname = usePathname();
-  // const router = useRouter();
+  const router = useRouter();
   const handleSave = async () => {
     await toggleSaveQuestion({
       userId: JSON.parse(userId),
@@ -84,6 +84,12 @@ const Votes = ({
       // todo show a toat
     }
   };
+  useEffect(() => {
+    viewQuestion({
+      questionId: JSON.parse(itemId),
+      userId: userId ? JSON.parse(userId) : undefined,
+    });
+  }, [itemId, userId, pathname, router]);
   return (
     <div className='flex gap-5'>
       <div className='flex-center gap-2.5'>
