5e24b2ee1d0d5a2ad07232007acd057566279b06
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-06-28 18:43:07 -0400

    !!

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,6 @@ export const metadata: Metadata = {
   icons: {
     icon: '/assets/images/site-logo.svg',
   },
-  openGraph: {
-    images: '/assets/images/demo.jpg',
-  },
 };
 export default function RootLayout({
   children,
